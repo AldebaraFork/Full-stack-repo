bac902e1fa72732221e4a4a87d6ba06c95c1f091
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2025-02-27 00:04:03 -0300

    Add: criar (cria uma nova categoria, utilizando uma requisição via postman)

--- src/main/java/com/dex/moneyapi/moneyapi/resource/CategoriaResource.java
+++ src/main/java/com/dex/moneyapi/moneyapi/resource/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.dex.moneyapi.moneyapi.resource;
 
+
 import com.dex.moneyapi.moneyapi.model.Categoria;
 import com.dex.moneyapi.moneyapi.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,21 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar(){
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+          Categoria categoriaSalva = categoriaRepository.save(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString ());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarCodigo(@PathVariable Long codigo){
+        return categoriaRepository.findById(codigo).get();
+
     }
 }
