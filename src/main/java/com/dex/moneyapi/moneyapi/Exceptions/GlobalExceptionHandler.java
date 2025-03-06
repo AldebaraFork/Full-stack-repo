7f05570e919815612af72afaee7b05b14c9f03ba
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2025-03-06 20:44:48 -0300

    Tratando mais Exceptions
    Add- Tratamento de mais exceptions, e mudança de herança na GlobalExceptionHandler

--- src/main/java/com/dex/moneyapi/moneyapi/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dex/moneyapi/moneyapi/Exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,26 @@
 package com.dex.moneyapi.moneyapi.Exceptions;
+import org.aspectj.bridge.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Locale;
 
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Autowired
+  private MessageSource messageSource;
 
 
   //retorna um 404 e captura a CategoriaNotFoundException
@@ -21,5 +35,10 @@ public class GlobalExceptionHandler extends RuntimeException {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro inesperado: " + ex.getMessage());
   }
 
-
+  // Caso a mensagem não possam ser lidas
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    String mensagemUsuario = messageSource.getMessage("mensagem.invalida",null, LocaleContextHolder.getLocale());
+    return handleExceptionInternal(ex, mensagemUsuario, headers, HttpStatus.BAD_REQUEST, request);
+  }
 }
